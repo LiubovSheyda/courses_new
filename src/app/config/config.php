1a4aa3e0dd07ae33ac2ba6f61bfe0775d442faf4
LiubovSheyda <liubovsheyda@gmail.com>
2016-10-29 17:33:41 +0700

    Good

---
+++ src/app/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+	'db' => [
+		'dsn' => 'mysql:host=localhost;dbname=publications',
+		'username' => 'root',
+		'password' => 'vagrant'
+	],
+	'router' => [
+		'/' => [
+			'controller' => 'IndexController',
+			'action' => 'indexAction'
+		],
+		'/about' => [
+			'controller' => 'IndexController',
+			'action' => 'aboutAction'
+		],
+		'/tasks/([a-zA-Z0-9]{1,})/([0-9]{1,})' => [
+			'controller' => 'TaskController',
+			'action' => 'indexAction',
+			'parameters' => ['section', 'task']
+		]
+	],
+	'path_to_views' => '/src/app/tasksApp/view/'
+];
+
+return $config;
